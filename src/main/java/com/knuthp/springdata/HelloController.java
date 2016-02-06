34d3658f7587e071f573bf5abac97007c2ed3413
Knut H. Pedersen <knuthp@gmail.com>
2016-02-06 19:41:05 +0100

    Adding Spring REST error handling

--- src/main/java/com/knuthp/springdata/HelloController.java
+++ src/main/java/com/knuthp/springdata/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/hello")
@@ -24,4 +26,27 @@ public class HelloController {
 		hello.add(linkTo(methodOn(HelloController.class).hello(name)).withSelfRel());
 		return new ResponseEntity<>(hello, HttpStatus.OK);
 	}
+
+	@RequestMapping("/error")
+	public HttpEntity<Hello> error() {
+		throw new IllegalStateException("Not correct state");
+	}
+
+	@ExceptionHandler(IllegalStateException.class)
+	public ResponseEntity<String> handleException(IllegalStateException ex) {
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	}
+
+	@RequestMapping("/error2")
+	public HttpEntity<Hello> error2() {
+		throw new IllegalArgumentException("Not correct argument");
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+
+	public String handleException2(IllegalArgumentException ex) {
+		return ex.getMessage();
+	}
+
 }
